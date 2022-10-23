7611e5e8cff1dff60b377e4df4193da9689cbebe
oluwaseun <adeshinaeo@gmail.com>
2022-10-23 01:28:19 -0700

    task putchar c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - prints putchar
- * Return: always 0
+ * main - prints _putchar and a new line
+ * Return: Always 0
  */
-int main(voi)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,1 @@
-0 Write a program that prints _putchar, followed by a new line
-The program should return 0
+0x02-functions_nested_loops
